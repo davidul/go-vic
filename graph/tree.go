6b544b09a16d70352cba566bba57580281403a5d
David Ulicny <ulicny.david@gmail.com>
2022-09-13 08:14:08 +0200

    Tree generic

---
+++ graph/tree.go
@@ -0,0 +1,76 @@
+package graph
+
+import (
+	"fmt"
+	"github.com/davidul/go-vic/linkedlist"
+)
+
+type TreeNode struct {
+	value    any
+	children *linkedlist.LinkedList
+}
+
+type Tree struct {
+	root TreeNode
+}
+
+func NewTree(value any) *Tree {
+	return &Tree{
+		root: TreeNode{
+			value:    value,
+			children: &linkedlist.LinkedList{},
+		},
+	}
+}
+
+func (T *Tree) Add(value any) *TreeNode {
+	return T.root.Add(value)
+}
+
+func (T *Tree) Root() *TreeNode {
+	return &T.root
+}
+
+func (TN *TreeNode) Add(value any) *TreeNode {
+	tn := TreeNode{
+		value:    value,
+		children: &linkedlist.LinkedList{},
+	}
+	TN.children.Add(tn)
+	return &tn
+}
+
+func (T *Tree) InOrder() {
+	T.inOrder(T.root.children)
+	fmt.Println(T.root.value)
+}
+
+func (T *Tree) inOrder(nodes *linkedlist.LinkedList) {
+	for x := nodes.Head(); x != nil; x = x.Next() {
+		T.inOrder(x.Data().(TreeNode).children)
+		fmt.Println(x.Data().(TreeNode).value)
+	}
+
+}
+
+func (T *Tree) PostOrder() {
+	fmt.Println(T.root.value)
+}
+
+func (T *Tree) postOrder(nodes *linkedlist.LinkedList) {
+	for x := nodes.Head(); x != nil; x = x.Next() {
+		if x.Next() != nil {
+			T.inOrder(x.Data().(TreeNode).children)
+		}
+		fmt.Println(x.Data().(TreeNode).value)
+	}
+}
+
+func Compare(t1 *Tree, t2 Tree) {
+	/*tr1 := t1.root.value
+	tr2 := t2.root.value
+	if tr1 == tr2 {
+		t1.root.children
+		t2.root.children
+	}*/
+}
