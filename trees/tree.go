4afd48ffffd102a6b0e20655db9dcba1001949d0
David Ulicny <ulicny.david@gmail.com>
2022-12-05 16:37:39 +0100

    Tree generics

--- trees/tree.go
+++ trees/tree.go
@@ -6,61 +6,61 @@ import (
 	"math"
 )
 
-type TreeNode struct {
+type TreeNode[T interface{}] struct {
 	Value    any
-	Children *linkedlist.LinkedList
+	Children *linkedlist.LinkedList[T]
 }
 
-type Tree struct {
-	root TreeNode
+type Tree[T interface{}] struct {
+	root TreeNode[T]
 }
 
-func NewTree(value any) *Tree {
-	return &Tree{
-		root: TreeNode{
+func NewTree[T interface{}](value any) *Tree[T] {
+	return &Tree[T]{
+		root: TreeNode[T]{
 			Value:    value,
-			Children: &linkedlist.LinkedList{},
+			Children: &linkedlist.LinkedList[T]{},
 		},
 	}
 }
 
-func (T *Tree) Add(value any) *TreeNode {
-	return T.root.Add(value)
+func (R *Tree[T]) Add(value T) *TreeNode[T] {
+	return R.root.Add(value)
 }
 
-func (T *Tree) Root() *TreeNode {
-	return &T.root
+func (R *Tree[T]) Root() *TreeNode[T] {
+	return &R.root
 }
 
 // Add new TreeNode to the tree.
-func (TN *TreeNode) Add(value any) *TreeNode {
-	tn := TreeNode{
+func (TN *TreeNode[T]) Add(value any) *TreeNode[T] {
+	tn := TreeNode[T]{
 		Value:    value,
-		Children: &linkedlist.LinkedList{},
+		Children: &linkedlist.LinkedList[T]{},
 	}
 	TN.Children.Add(tn)
 	return &tn
 }
 
-func (TN *TreeNode) Delete() {
+func (TN *TreeNode[T]) Delete() {
 
 }
 
 // Bsf - Breadth First Search on the tree
-func (T *Tree) Bsf(goal any) *TreeNode {
-	visited := linkedlist.LinkedList{}
-	queue := linkedlist.LinkedList{}
-	queue.Add(T.root)
+func (R *Tree[T]) Bsf(goal any) *TreeNode[T] {
+	visited := linkedlist.LinkedList[T]{}
+	queue := linkedlist.LinkedList[T]{}
+	queue.Add(R.root)
 
 	for !queue.IsEmpty() {
 		v := queue.Poll()
 		visited.Add(v)
-		vv := v.(TreeNode).Value
+		vv := v.(TreeNode[T]).Value
 		if vv == goal {
-			treeNode := v.(TreeNode)
+			treeNode := v.(TreeNode[T])
 			return &treeNode
 		}
-		node := v.(TreeNode)
+		node := v.(TreeNode[T])
 		list := node.Children
 		for e := list.Head(); e != nil; e = e.Next() {
 			if !visited.Contains(e.Data()) {
@@ -71,64 +71,64 @@ func (T *Tree) Bsf(goal any) *TreeNode {
 	return nil
 }
 
-func (T *Tree) PostOrder() {
-	T.postOrder(T.root.Children)
-	fmt.Println(T.root.Value)
+func (R *Tree[T]) PostOrder() {
+	R.postOrder(R.root.Children)
+	fmt.Println(R.root.Value)
 }
 
-func (T *Tree) postOrder(nodes *linkedlist.LinkedList) {
+func (R *Tree[T]) postOrder(nodes *linkedlist.LinkedList[T]) {
 	for x := nodes.Head(); x != nil; x = x.Next() {
-		T.postOrder(x.Data().(TreeNode).Children)
-		fmt.Println(x.Data().(TreeNode).Value)
+		R.postOrder(x.Data().(TreeNode[T]).Children)
+		fmt.Println(x.Data().(TreeNode[T]).Value)
 	}
 }
 
-func (T *Tree) PreOrder() {
-	fmt.Println(T.root.Value)
-	T.preOrder(T.root.Children, 1, math.MaxInt)
+func (R *Tree[T]) PreOrder() {
+	fmt.Println(R.root.Value)
+	R.preOrder(R.root.Children, 1, math.MaxInt)
 }
 
-func (T *Tree) preOrder(nodes *linkedlist.LinkedList, lvl int, stopLvl int) {
+func (R *Tree[T]) preOrder(nodes *linkedlist.LinkedList[T], lvl int, stopLvl int) {
 	lvl += 1
 	if stopLvl <= lvl {
 		return
 	}
 	for x := nodes.Head(); x != nil; x = x.Next() {
-		fmt.Println(x.Data().(TreeNode).Value)
-		T.preOrder(x.Data().(TreeNode).Children, lvl, stopLvl)
+		fmt.Println(x.Data().(TreeNode[T]).Value)
+		R.preOrder(x.Data().(TreeNode[T]).Children, lvl, stopLvl)
 	}
 }
 
-func (T *Tree) PreOrderDepth(depth int) {
-	fmt.Println(T.root.Value)
-	T.preOrder(T.root.Children, 0, depth)
+func (R *Tree[T]) PreOrderDepth(depth int) {
+	fmt.Println(R.root.Value)
+	R.preOrder(R.root.Children, 0, depth)
 }
 
-func (T *Tree) PreOrderDepthFunc(depth int, f func(any)) {
-	f(T.root.Value)
-	T.preOrderFunc(T.root.Children, 0, depth, f)
+func (R *Tree[T]) PreOrderDepthFunc(depth int, f func(any)) {
+	f(R.root.Value)
+	R.preOrderFunc(R.root.Children, 0, depth, f)
 }
 
-func (T *Tree) preOrderFunc(nodes *linkedlist.LinkedList, lvl int, stopLvl int, f func(any)) {
+func (R *Tree[T]) preOrderFunc(nodes *linkedlist.LinkedList[T], lvl int, stopLvl int, f func(any)) {
 	lvl += 1
 	if stopLvl <= lvl {
 		return
 	}
 	for x := nodes.Head(); x != nil; x = x.Next() {
-		f(x.Data().(TreeNode).Value)
-		T.preOrder(x.Data().(TreeNode).Children, lvl, stopLvl)
+		f(x.Data().(TreeNode[T]).Value)
+		R.preOrder(x.Data().(TreeNode[T]).Children, lvl, stopLvl)
 	}
 }
 
-func Compare(t1 *Tree, t2 Tree) {
-	/*tr1 := t1.root.value
-	tr2 := t2.root.value
-	if tr1 == tr2 {
-		t1.root.Children
-		t2.root.Children
-	}*/
-}
+//func Compare(t1 *Tree, t2 Tree) {
+/*tr1 := t1.root.value
+tr2 := t2.root.value
+if tr1 == tr2 {
+	t1.root.Children
+	t2.root.Children
+}*/
+//}
 
-func (T *Tree) Walk() {
+func (R *Tree[T]) Walk() {
 
 }
